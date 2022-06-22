5bcd2b3facf3d3d63607dc5a68b59411a85c0b92
clumsy <clumsy.xx@gmail.com>
2022-06-22 11:35:24 +0200

    1

--- js/audio.js
+++ js/audio.js
@@ -1,8 +1,9 @@
 var enter = document.getElementById('enter');
-var audio = document.getElementById('audio')
+var audio = document.getElementById('audio');
 enter.onclick = function () {
     if (enter.style.opacity !== '0') {
         enter.style.opacity = '0';
+        audio.volume = 0.5;
         audio.play();
         setTimeout(function () {
             enter.remove();
